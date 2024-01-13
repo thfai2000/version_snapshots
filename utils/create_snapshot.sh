39fe14e482cc12cff3122e93cd84ccf80c9dc680
Eddie Tang <thfai2000@gmail.com>
2024-01-13 16:40:32 +0800

    fix

--- utils/create_snapshot.sh
+++ utils/create_snapshot.sh
@@ -2,13 +2,16 @@
 
 yaml_folder="$1"
 snapshot_name="$2"
-postfix="1"
+rc_number="2"
 
 destination_file="$yaml_folder/$snapshot_name.yaml"
 
 while [[ -e "$destination_file" ]]; do
-  postfix="RC$((postfix + 1))"
+  postfix="RC$((rc_number++))"
   destination_file="$yaml_folder/${snapshot_name}_${postfix}.yaml"
+
+  echo "Test file - $destination_file"
 done
 
+echo "Creating file - $destination_file"
 cp "$yaml_folder/latest.yaml" "$destination_file"
