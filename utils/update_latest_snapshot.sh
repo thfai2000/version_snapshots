8f397dfca5b5d685597ff9f3b56ac0ccde4f98bf
Eddie Tang <thfai2000@gmail.com>
2024-01-13 13:02:38 +0800

    fix

--- utils/update_latest_snapshot.sh
+++ utils/update_latest_snapshot.sh
@@ -1,31 +1,25 @@
 #!/bin/bash
 
 # Function to update the version and artifact path based on the name
-update_yaml() {
-  local yaml_file="$1"
-  local name="$2"
-  local version="$3"
-  local artifact_path="$4"
 
-  if [[ ! -f "$yaml_file" ]]; then
-    echo "name: $name" > "$yaml_file"
-    echo "version: $version" >> "$yaml_file"
-    echo "artifact_path: $artifact_path" >> "$yaml_file"
-  else
-    if ! yq eval --exit-status 'select(.name == "'"$name"'")' "$yaml_file"; then
-      echo "name: $name" >> "$yaml_file"
-      echo "version: $version" >> "$yaml_file"
-      echo "artifact_path: $artifact_path" >> "$yaml_file"
-    else
-      yq eval --inplace 'select(.name == "'"$name"'") | .version = "'"$version"'" | .artifact_path = "'"$artifact_path"'"' "$yaml_file"
-    fi
-  fi
-}
-
-# Usage: ./update_yaml_script.sh <yaml_folder> <name> <version> <artifact_path>
-yaml_folder="$1"
+yaml_file="$1/latest.yaml"
 name="$2"
 version="$3"
-artifact_path="$4"
+commit_sha="$4"
+artifact_path="$5"
 
-update_yaml "$yaml_folder/latest.yaml" "$name" "$version" "$artifact_path"
+if [[ ! -f "$yaml_file" ]]; then
+  echo "name: $name" > "$yaml_file"
+  echo "version: $version" >> "$yaml_file"
+  echo "commit_sha: $commit_sha" >> "$yaml_file"
+  echo "artifact_path: $artifact_path" >> "$yaml_file"
+else
+  if ! yq eval --exit-status 'select(.name == "'"$name"'")' "$yaml_file"; then
+    echo "name: $name" >> "$yaml_file"
+    echo "version: $version" >> "$yaml_file"
+    echo "commit_sha: $commit_sha" >> "$yaml_file"
+    echo "artifact_path: $artifact_path" >> "$yaml_file"
+  else
+    yq eval --inplace 'select(.name == "'"$name"'") | .version = "'"$version"'" | .commit_sha = "'"$commit_sha"'" | .artifact_path = "'"$artifact_path"'"' "$yaml_file"
+  fi
+fi
